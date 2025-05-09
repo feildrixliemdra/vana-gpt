6e19266d65394d1c2d70dc4d0a07c4e294f6548f
feildrixliemdra <feildrixliemdra@gmail.com>
2025-05-09 22:08:24 +0700

    en: update model to use deepseek

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -3,7 +3,7 @@ import {
   extractReasoningMiddleware,
   wrapLanguageModel,
 } from 'ai';
-import { xai } from '@ai-sdk/xai';
+import { deepseek } from '@ai-sdk/deepseek';
 import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
@@ -23,15 +23,15 @@ export const myProvider = isTestEnvironment
     })
   : customProvider({
       languageModels: {
-        'chat-model': xai('grok-2-vision-1212'),
+        'chat-model': deepseek('deepseek-chat'),
         'chat-model-reasoning': wrapLanguageModel({
-          model: xai('grok-3-mini-beta'),
+          model: deepseek('deepseek-reasoner'),
           middleware: extractReasoningMiddleware({ tagName: 'think' }),
         }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
+        'title-model': deepseek('deepseek-chat'),
+        'artifact-model': deepseek('deepseek-chat'),
       },
-      imageModels: {
-        'small-model': xai.image('grok-2-image'),
-      },
+      // imageModels: {
+      //   'small-model': xai.image('grok-2-image'),
+      // },
     });
